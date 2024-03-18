2e07d7ab5014e142eff342776b8b41d4400146a1
tbrown034 <tbrown034@gmail.com>
2024-03-17 21:58:18 -0500

    new

--- app/UI/Map/MapSection.jsx
+++ app/UI/Map/MapSection.jsx
@@ -9,9 +9,9 @@ const MapSection = () => {
         Discover <span className="text-teal-500"> Comfort & Care</span> at Our
         Naperville Dental Home
       </h2>
-      <p className="mb-8 text-center text-gray-500 sm:text-xl">
-        For over 30 years, we've dedicated ourselves to bringing smiles to life
-        right here in Naperville. Enjoy the panoramic views and exceptional
+      <p className="mb-8 text-center sm:text-xl">
+        For over 30 years, we have dedicated ourselves to bringing smiles to
+        life right here in Naperville. Enjoy the panoramic views and exceptional
         dental care on the third floor of the Fifth Third Bank building at 75th
         and Rickert.
       </p>
