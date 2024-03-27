e5d4dd19eba1361ed8ffa367c45aa7a4ded91496
tbrown034 <tbrown034@gmail.com>
2024-03-27 04:05:53 -0400

    new grid

--- app/UI/DentalServices/DentalServicesImgGroup.jsx
+++ app/UI/DentalServices/DentalServicesImgGroup.jsx
@@ -1,21 +1,17 @@
 import Image from "next/image";
 
-import officeChairWithDoll from "../../../public/images/office/officeChairWithDoll.jpeg";
 import Nitrous from "../../../public/images/office/officeEquipmentNitrous.jpeg";
-import verticalOffice from "../../../public/images/office/officeChairWithViewVertical.jpeg";
+import officeStuff from "../../../public/images/office/officeEquipmentPc1000.jpeg";
 
 const DentalServicesImgGroup = () => {
   return (
     <div className="grid grid-cols-2 gap-4">
       <div className="flex items-center rounded-xl">
-        <Image src={verticalOffice} alt="office ve" className="rounded-xl" />
+        <Image src={Nitrous} alt="office ve" className="rounded-xl" />
       </div>
       <div className="flex flex-col gap-4">
-        <div className="">
-          <Image src={Nitrous} alt="nitrous" className="rounded-xl" />
-        </div>
         <div className=" rounded-xl">
-          <Image src={officeChairWithDoll} alt="dolls" className="rounded-xl" />
+          <Image src={officeStuff} alt="dolls" class Name="rounded-xl" />
         </div>
       </div>
     </div>
