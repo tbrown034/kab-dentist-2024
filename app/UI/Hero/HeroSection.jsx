1cfb227fe5a89346c12a43504c26081cfa93a624
tbrown034 <tbrown034@gmail.com>
2024-04-12 17:27:57 -0400

    WIP: responsive hero section

--- app/UI/Hero/HeroSection.jsx
+++ app/UI/Hero/HeroSection.jsx
@@ -6,17 +6,21 @@ import HeroCTA from "./HeroCTA";
 const HeroSection = () => {
   const { title, highlightedText, highlightInFront } = sectionContents.heroText;
   return (
-    <section className="flex flex-col gap-4">
-      <div className="text-3xl font-extrabold leading-tight tracking-tight md:text-4xl">
-        <FullTitle
-          title={title}
-          highlightedText={highlightedText}
-          highlightInFront={highlightInFront}
-        />
+    <section className="flex flex-col gap-4 md:flex-row">
+      <div className="flex flex-col justify-center gap-4 md:gap-8 xl:gap-12 2xl:gap-16 md:w-3/5">
+        <div className="text-3xl font-extrabold leading-tight tracking-tight xl:text-5xl 2xl:text-6xl md:text-4xl">
+          <FullTitle
+            title={title}
+            highlightedText={highlightedText}
+            highlightInFront={highlightInFront}
+          />
+        </div>
+        <HeroBody />
+        <HeroCTA />
       </div>
-      <HeroBody />
-      <HeroCTA />
-      <HeroImgSlider />
+      <div className=" md:w-2/5">
+        <HeroImgSlider />
+      </div>
     </section>
   );
 };
