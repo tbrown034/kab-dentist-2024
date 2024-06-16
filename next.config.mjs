7ff61b38781bbcf6f630d4fc82db8e2b96983cd9
tbrown034 <tbrown034@gmail.com>
2024-06-16 13:20:13 -0400

    static sitemap

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: "/sitemap.xml",
-        destination: "/sitemap.js",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
