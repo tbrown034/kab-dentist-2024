3b6f4daf5753cf67ab7054dc70fb5b260d75838e
tbrown034 <tbrown034@gmail.com>
2024-08-05 18:12:02 -0400

    WIP: forms works, slider unstable and DialogCheck comes up no matter what

---
+++ app/UI/Forms/FormInputs.jsx
@@ -0,0 +1,137 @@
+import React from "react";
+
+export const NameInput = ({ value, onChange }) => (
+  <div>
+    <label htmlFor="name" className="block mb-2 font-medium">
+      Name
+    </label>
+    <input
+      name="name"
+      type="text"
+      id="name"
+      value={value}
+      onChange={onChange}
+      className="block w-full p-3 text-black border border-gray-300 rounded-lg shadow-sm focus:ring-teal-500 focus:border-teal-500"
+      placeholder="John Doe"
+      required
+    />
+  </div>
+);
+
+export const EmailInput = ({ value, onChange }) => (
+  <div>
+    <label htmlFor="email" className="block mb-2 font-medium">
+      Email
+    </label>
+    <input
+      name="email"
+      type="email"
+      id="email"
+      value={value}
+      onChange={onChange}
+      className="block w-full p-3 text-black border border-gray-300 rounded-lg shadow-sm focus:ring-teal-500 focus:border-teal-500"
+      placeholder="email@example.com"
+      required
+    />
+  </div>
+);
+
+export const PhoneInput = ({ value, onChange }) => (
+  <div>
+    <label htmlFor="phone" className="block mb-2 font-medium">
+      Phone
+    </label>
+    <input
+      name="phone"
+      type="tel"
+      id="phone"
+      value={value}
+      onChange={onChange}
+      className="block w-full p-3 text-black border border-gray-300 rounded-lg shadow-sm focus:ring-teal-500 focus:border-teal-500"
+      placeholder="(555) 555-5555"
+      required
+    />
+  </div>
+);
+
+export const CityInput = ({ value, onChange }) => (
+  <div>
+    <label htmlFor="city" className="block mb-2 font-medium">
+      City
+    </label>
+    <input
+      name="city"
+      type="text"
+      id="city"
+      value={value}
+      onChange={onChange}
+      className="block w-full p-3 text-black border border-gray-300 rounded-lg shadow-sm focus:ring-teal-500 focus:border-teal-500"
+      placeholder="Naperville"
+      required
+    />
+  </div>
+);
+
+export const InsuranceInput = ({ value, onChange }) => (
+  <div>
+    <label htmlFor="insurance" className="block mb-2 font-medium">
+      Insurance <DialogBox />
+    </label>
+    <input
+      name="insurance"
+      type="text"
+      id="insurance"
+      value={value}
+      onChange={onChange}
+      className="block w-full p-3 mt-2 text-black border border-gray-300 rounded-lg shadow-sm focus:ring-teal-500 focus:border-teal-500"
+      placeholder="Insurance Provider"
+      required
+    />
+  </div>
+);
+
+export const QuestionInput = ({ value, onChange }) => (
+  <div>
+    <label htmlFor="question" className="block mb-2 font-medium">
+      Question/Issue
+    </label>
+    <textarea
+      name="question"
+      id="question"
+      value={value}
+      onChange={onChange}
+      className="block w-full p-3 text-black border border-gray-300 rounded-lg shadow-sm focus:ring-teal-500 focus:border-teal-500"
+      rows="3"
+      placeholder="Please describe your question or issue."
+      required
+    ></textarea>
+  </div>
+);
+
+export const ReturningPatientInput = ({ value, onChange }) => (
+  <div>
+    <label htmlFor="returningPatient" className="block mb-2 font-medium">
+      Returning Patient?
+    </label>
+    <div className="flex items-center gap-2">
+      <input
+        name="returningPatient"
+        type="radio"
+        id="returningPatientYes"
+        value="yes"
+        checked={value === "yes"}
+        onChange={onChange}
+      />
+      Yes
+      <input
+        name="returningPatient"
+        type="radio"
+        id="returningPatientNo"
+        value="no"
+        checked={value === "no"}
+        onChange={onChange}
+      />
+      No
+    </div>
+  </div>
+);
--- app/error.jsx
+++ app/error.jsx
@@ -1,104 +1,36 @@
 "use client";
-// app/make-an-appointment/page.jsx
-import React from "react";
-import AppointmentForm from "../app/UI/Appointment/AppointmentForm";
-import { raleway } from "../app/font.js";
-import FullTitle from "../app/UI/Other/FullTitle";
-import sectionContents from "../sectionContent.json";
+
 import Link from "next/link";
+import Image from "next/image";
+import confusedTooth from "../public/images/logos/confusedTooth.png";
 
-export const metadata = {
-  title: "Make an Appointment | Dr. Keith Brown DDS",
-  description:
-    "Schedule your appointment with Dr. Keith Brown, DDS, FAGD, today. Fill out the form to get started.",
-  keywords: "appointment, dentist, Keith Brown DDS, Naperville, dental care",
-  openGraph: {
-    title: "Make an Appointment | Keith Brown DDS",
-    description:
-      "Schedule your appointment with Dr. Keith Brown, DDS, FAGD, today. Fill out the form to get started.",
-    url: "https://keithbrowndds.com/make-an-appointment",
-    images: [
-      {
-        url: "https://keithbrowndds.com/og-appointment.jpg",
-        width: 800,
-        height: 600,
-        alt: "Keith Brown DDS - Make an Appointment",
-      },
-    ],
-    site_name: "Keith Brown DDS",
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Make an Appointment | Keith Brown DDS",
-    description:
-      "Schedule your appointment with Dr. Keith Brown, DDS, FAGD, today. Fill out the form to get started.",
-    image: "https://keithbrowndds.com/twitter-appointment.jpg",
-  },
-};
-
-const MakeAnAppointmentPage = () => {
-  const { title, textBlock, highlightedText, highlightInFront } =
-    sectionContents.makeAnAppointmentSection;
-
-  // Ensure there's content to display, otherwise return null
-  if (!title || !textBlock || textBlock.length === 0) return null;
-
+export default function ErrorPage() {
   return (
-    <section className="flex flex-col gap-4 px-4 mt-6" id="appointmentSection">
-      <h1
-        className={`${raleway.className} text-2xl md:text-3xl font-extrabold tracking-tight`}
-      >
-        <FullTitle
-          title={title}
-          highlightedText={highlightedText}
-          highlightInFront={highlightInFront}
-        />
-      </h1>
-      <div className="flex flex-col gap-4">
-        {textBlock.map((block, blockIndex) => (
-          <p key={blockIndex} className="text-lg">
-            {block.text}
-          </p>
-        ))}
-      </div>
-      <div className="flex flex-row items-center gap-4 py-2">
-        <Link
-          href="#appointmentForm"
-          className="p-2 text-sm text-white bg-teal-600 border-2 border-teal-600 rounded-lg hover:bg-teal-500 active:bg-teal-400"
-        >
-          Appointment Form
-        </Link>
-        <Link
-          href="/call-now"
-          className="p-2 text-sm text-white bg-teal-900 border-2 border-teal-900 rounded-lg hover:bg-teal-800 active:bg-teal-600"
-        >
-          Call Now
-        </Link>
-        <Link
-          href="/"
-          className="p-2 text-sm bg-white border-2 border-gray-400 rounded-lg hover:bg-gray-200 dark:text-black active:bg-gray-300"
-        >
-          Back to Home
-        </Link>
-      </div>
-      <div
-        id="appointmentForm"
-        className="flex flex-col gap-2 p-4 mx-4 text-white bg-teal-800 rounded-lg shadow px-text-white"
-      >
-        <h2 className="text-2xl font-extrabold tracking-tight">
-          Book Your Appointment
-        </h2>
-
-        <p>
-          Fill out the form below and we'll be in touch shortly to confirm your
-          appointment and answer any questions you might have!
+    <div className="flex flex-col items-center justify-center gap-2 p-6">
+      <div className="flex flex-col items-center">
+        <h1 className="text-2xl font-bold text-red-600">
+          Something went wrong
+        </h1>
+        <p className="mt-4 text-lg text-gray-700">
+          We are sorry, but something went wrong on our end. Please try again
+          later.
         </p>
-        <AppointmentForm />
       </div>
-    </section>
+      <div className="p-10">
+        <Image
+          src={confusedTooth}
+          alt="Confused Tooth"
+          width={300}
+          height={300}
+          className="rounded-2xl"
+        />
+      </div>
+      <Link
+        href="/"
+        className="p-8 text-lg text-white bg-teal-600 rounded-xl hover:bg-teal-500"
+      >
+        Go Back Home
+      </Link>
+    </div>
   );
-};
-
-export default MakeAnAppointmentPage;
+}
